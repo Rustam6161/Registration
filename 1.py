ebd85bccc29b1e649d12ab5c3795b49e36ae1729
Rustam <rustam@example.com>
2022-03-12 16:25:38 +0500

    salom

---
+++ 1.py
@@ -0,0 +1,132 @@
+
+
+
+    # 1-masala;
+
+
+# for i in range(int(input()),0,-1):
+#     for k in range(i):
+#         print(" *", end = ' ')
+#     print()
+
+
+
+
+    # 2-masala;
+
+
+# son = input()
+# son = son[::-1]
+#
+# print(son)
+
+
+    # 3-masala;
+
+# gap = str(input())
+# son = str(input())
+# new =len(gap)//2
+#
+# print(gap[:new]+son+gap[new:])
+
+
+    # 4-masala;
+
+# gap = str(input("So'z kiriting: "))
+#
+# print(gap.strip().lower())
+
+    # 5-masala;
+#
+# prize = 'PSG is a good club'
+#
+# for i in prize.split()[::-1]:
+#     print(i, end=' ')
+
+    # 6-masala;
+#
+# [print(i, end = ' ') for i in range(20,100) if i % 7 == 0]
+
+    # 7-masala;
+
+# [print(i, end = ' ') for i in range(100,20,-1) if i % 7 == 0]
+
+    # 8-masala;
+
+# def pal(string):
+#     return 'Palindrome' if string == string[::-1] else 'Not palindrome'
+#
+# print(pal(str(input())))
+
+
+    # 9-masala;
+
+# print(list(map(lambda a: a*a, map(int,list(input().split())))))
+
+    # 10-masala;
+
+# def bir_xil(li1, li2):
+#     return [int(i) for i in li1 if i in li2]
+#
+#
+# list1 = list(input().split())
+# list2 = list(input().split())
+#
+# print(bir_xil(list1,list2))
+
+    # 11-masala;
+
+# def har_xil(le1, le2):
+#     le1+=le2
+#     return [int(i) for i in le1 if le1.count(i)==1]
+#
+#
+# list1 = list(input().split())
+# list2 = list(input().split())
+#
+# print((har_xil(list1,list2)))
+
+    # 12-masala;
+
+# def sana(son):
+#     return len(set(list1))
+#
+#
+# list1 = [1,2,3,4,5,3,4,6,7,8]
+#
+# print(sana(list1))
+
+    # 13-masala;
+
+# args-arguments(argumentlar) bu, funktsiyaga kiritiluvchi o'zgaruvchilarning miqdori noaniq bo'lgan holatda qo'llaniladi
+# natijada functsiyamizga hohlagancha argumentlarni(o'zgaruvchilarni) kirita olamiz.
+
+# 'args'ning sintaksisi quyidagicha:
+
+# def fun(*sonlar):
+#     return sonlar
+#
+# a,b,c,d,e = 1,2,3,4,5
+# print(fun(a,b,c,d,e))
+
+
+    # 14-masala;
+
+# kwargs-key-words-arguments(kalit-so'z-argumentlari) bu, funktsiyaga kiritiluvchi kalit so'zlarning miqdori noaniq bo'lgan holatda qo'llaniladi
+# demak 'kwargs' faqat dictionary(lug'atlar)gagina ishlaydi
+# natijada functsiyamizga hohlagancha kalit so'zlarni kirita olamiz.
+
+
+#________________E'tiboringiz uchun RAXMAT1________________________________
+
+
+
+
+
+
+
+
+
+
+
+
